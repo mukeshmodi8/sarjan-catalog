3d70f21de1cd4e4183b794f35a7c789a7b443d97
mukeshmodi8 <mvmodi.2014@gmail.com>
2025-11-27 15:46:47 +0530

    new code update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,9 @@ import React, {
 import axios from "axios";
 
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://sarjan-catalog.onrender.com/api";
+
+
 
 
 const ADMIN_PASSWORD = "12345";
